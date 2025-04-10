ec2ae3b7949c1aeaa7fca8830eb3fe9f6cee5b3d
Ditar <DitarRogozhnikov@gmail.com>
2025-04-10 17:47:35 +0300

    InitialCommit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.micro"
+version = "0.0.1"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	//Eureka
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.2.1")
+
+	//Addiction
+	implementation("io.micrometer:micrometer-registry-prometheus:1.14.5")
+	implementation("org.springframework.boot:spring-boot-starter-actuator:3.4.4")
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway:4.2.2")
+	implementation("org.springframework.boot:spring-boot-devtools:3.4.4")
+	implementation("org.springframework.boot:spring-boot-configuration-processor:3.4.4")
+
+	compileOnly("org.projectlombok:lombok:1.18.38")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
